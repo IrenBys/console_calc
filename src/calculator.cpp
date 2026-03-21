c4952db26929fe62d7b1ec8ea843489657c6bee3
Ирина <i.bystrova@bms-cloud.ru>
2026-03-22 00:43:01 +0300

    статусы ошибок

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,32 +1,42 @@
 #include "calculator.h"
 #include "mathlib.h"
 
+
+// Устанавливаем статус и при необходимости выводим подсказку
+void setTaskStatus(Task& task, mathlib::Status status) 
+{
+    task.status = status;
+    if (status != mathlib::OK) 
+    {
+        fprintf(stderr, "See --help for usage\n");
+    }
+}
+
 // Функция для выполнения вычислений в зависимости от операции, указанной в структуре Task.
 // Результат сохраняется в поле result, а статус ошибки (если есть) сохраняется в поле status.
-void calculate(Task& task)
-{
-    task.status = 0;
-    switch (task.operation)
-    {
-    case '+':
-        task.status = mathlib::addition(task.value1, task.value2, &task.result);
-        break;
-    case '-':
-        task.status = mathlib::substraction(task.value1, task.value2, &task.result);
-        break;
-    case '*':
-        task.status = mathlib::multiplication(task.value1, task.value2, &task.result);
-        break;
-    case '/':
-        task.status = mathlib::division(task.value1, task.value2, &task.result);
-        break;
-    case '^':
-        task.status = mathlib::power(task.value1, task.value2, &task.result);
-        break;
-    case '!':
-        task.status = mathlib::factorial(task.value1, &task.result);
-        break;
-    default:
-        task.status = 1;
+void calculate(Task& task) {
+    switch (task.operation) {
+        case '+':
+            setTaskStatus(task, mathlib::addition(task.value1, task.value2, &task.result));
+            break;
+        case '-':
+            setTaskStatus(task, mathlib::substraction(task.value1, task.value2, &task.result));
+            break;
+        case '*':
+            setTaskStatus(task, mathlib::multiplication(task.value1, task.value2, &task.result));
+            break;
+        case '/':
+            setTaskStatus(task, mathlib::division(task.value1, task.value2, &task.result));
+            break;
+        case '^':
+            setTaskStatus(task, mathlib::power(task.value1, task.value2, &task.result));
+            break;
+        case '!':
+            setTaskStatus(task, mathlib::factorial(task.value1, &task.result));
+            break;
+        default:
+            setTaskStatus(task, mathlib::ERROR);
+            fprintf(stderr, "Error! Unknown operation\n");
+            break;
     }
 }
