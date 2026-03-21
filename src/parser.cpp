0a82eb8a4d5179230e5e8b2e61f5f73444ba73c1
Ирина <i.bystrova@bms-cloud.ru>
2026-03-22 01:49:04 +0300

    правка парсера командной строки

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,96 +1,77 @@
 #include "parser.h"
-#include "printer.h"
-#include <cstdlib>
 #include <getopt.h>
 #include <cstdio>
+#include <cstdlib>
+#include "printer.h"
 
-
-// Обработка --help
-bool parseHelp(int opt, Task& task) 
+// Функция для установки статуса ошибки и печати сообщения об ошибке
+void setError(Task& task, const char* message) 
 {
-    if (opt == 'h') 
+    if (message)
     {
-        printHelp();    // Печатаем справку и устанавливаем статус, чтобы не выполнять дальнейшие действия
-        task.status = 2;
-        return true;  
+        fprintf(stderr, "Error: %s\n", message);
+    }
+    fprintf(stderr, "See --help for usage\n");
+    task.status = ERROR;
+}
+
+// Проверка --help
+bool parseHelp(int argc, char** argv, Task& task) 
+{
+    static struct option long_options[] = {
+        {"help", no_argument, 0, 'h'}, 
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "h", long_options, NULL)) != -1) 
+    {
+        if (opt == 'h') 
+        {
+            printHelp();
+            task.status = HELP;
+            return true;
+        }
     }
     return false;
 }
 
-// Проверка операций
-bool checkOperationArgs(int args_number, Task& task) 
+// Основная функция parse
+void parse(int argc, char** argv, Task& task) 
 {
-    // Для бинарных операций должно быть 3 аргумента: число1, операция, число2
-    if (args_number != 3) 
+    task.status = OK;
+
+    // 1. Проверяем --help
+    if (parseHelp(argc, argv, task)) 
     {
-        fprintf(stderr, "Binary operation requires two numbers\n");
-        task.status = 1;
-        return false;
-    }
-    return true;
-}
-
-
-// Проверка факториала 
-bool checkFactorialArgs(int args_number, Task& task) 
-{
-    // Для факториала должно быть 2 аргумента: число и операция
-    if (args_number != 2) {
-        fprintf(stderr, "Error: factorial requires only one number\n");
-        task.status = 1;
-        return false;
-    }
-    return true;
-}
-
-
-// Функция для парсинга аргументов командной строки и заполнения структуры Task
-void parse(int argc, char** argv, Task& task)
-{
-    // Изначально считаем, что статус "ok"
-    task.status = 0;
-    
-    // Определяем длинные опции для getopt_long
-    static struct option long_options[] = {
-        {"help", no_argument, 0, 'h'},
-        {0, 0, 0, 0}};
-
-    // Сначала проверяем, не вызван ли --help
-    int opt;  // Переменная для хранения текущей опции
-    while ((opt = getopt_long(argc, argv, "h", long_options, NULL)) != -1) 
-    {
-        if (parseHelp(opt, task)) 
-        {
-            return; // ��сли был вызван --help, то завершаем парсинг
-        }
-    }
-
-    // Если --help не был вызван, то продолжаем парсинг остальных аргуме��тов
-    if (optind >= argc) 
-    {
-        fprintf(stderr, "Error: No arguments provided\n");
-        task.status = 1;
         return;
     }
 
-    
-    task.value1 = atoi(argv[optind]);        // Парсим первое число
-    task.operation = *(argv[optind + 1]);    // Парсим операцию 
-
-    // Проверяем, является ли операция факториалом
-    if (task.operation == '!') 
-    {
-        if (!checkFactorialArgs(argc - optind, task)) 
-        {
-            return; // Если аргументы для факториала некорректные, то завершаем парсинг
-        }
-    } 
-    else 
-    {
-        if (!checkOperationArgs(argc - optind, task)) 
-        {
-            return; // Если аргументы для бинарной операции некорректные, то завершаем парсинг
-        }
-        task.value2 = atoi(argv[optind + 2]); // Парсим второе число для бинарных операций
+    // 2. Парсим первый аргумент
+    if (optind >= argc) {
+        setError(task, "Error: First number missing\n");
+        return;
     }
+    task.value1 = atoi(argv[optind]);
+  
+    // 3. Парсим операцию
+    if (optind + 1 >= argc) {
+        setError(task, "Error: Operation missing\n");
+        return;
+    }
+    task.operation = argv[optind + 1][0];
+
+
+    // 4. Парсим второй аргумент (если бинарная операция)
+    if (task.operation != '!') 
+    {
+        if (optind + 2 >= argc) {
+            setError(task, "Error: Second number missing\n");
+            return;
+        }
+        task.value2 = atoi(argv[optind + 2]);
+    }
+
+
+
 }
